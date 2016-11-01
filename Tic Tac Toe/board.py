93f6a08c65efc0d7762b49c758246d729a83dd05
Osher Lerner <oshlern@nuevaschool.org>
2016-10-31 20:04:01 -0700

    Created repo and began work on tic tac toe

---
+++ Tic Tac Toe/board.py
@@ -0,0 +1,7 @@
+class board:
+
+    def __init__(numPlayers=2, size=3, dimension=2):
+        state = [0] * size
+        for i in range(dimension):
+            state = [state] * size
+        self.state = state
