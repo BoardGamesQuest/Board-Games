d8e4471780f78dadb0ba02f2796cde31b0b9ca95
Osher Lerner <oshlern@nuevaschool.org>
2016-11-10 09:36:11 -0800

    Fixed state initialization and act

--- Tic Tac Toe/board.py
+++ Tic Tac Toe/board.py
@@ -1,36 +1,50 @@
 import numpy as np
 
-class board:
+class Board:
     # TODO:
     #   Make an efficient clone
     #   Standardize errors (like logkitten)
     #       Type errors (could we just expect that they don't happen? Only for debugging)
     #       Invalid moves
     # Move this To Do stuff into readme?
+    currentPlayer = 1
 
-    def __init__(numPlayers=2, size=3, dimension=2, debug=False):
+    def __init__(self, numPlayers=2, size=3, dimension=2, debug=False):
         self.numPlayers, self.size, self.dimension = numPlayers, size, dimension
-        self.state = np.zeros(*[size]*dimension)
+        self.state = np.zeros(tuple([size]*dimension), dtype=np.int)
 
-    def act(self, player, position):
-        if self.state[*position] != 0:
+    def display(self):
+        print self.state
+
+    def act(self, position, player=currentPlayer):
+        if type(position) == list:
+            position = tuple(position)
+        # print "Player {}, make your move.".format(player)
+        if self.state[position] != 0:
             print "Invalid Move: {} by Player {}".format(position, player)
             return
-        self.state[*position] = player
+        self.state[position] = player
+        print self.state[position]
+        self.currentPlayer = (self.currentPlayer % self.numPlayers) + 1
 
     def checkWin(self):
         rows = [] #generate rows
         diag1 = []
         diag2 = []
         for i in range(size):
-            rows.append(self.state(i,:))
-            rows.append(self.state(:,i))
-            diag1.append(self.state(i,i))
-            diag2.append(self.state(i,size-i))
+            rows.append(self.state[i,:])
+            rows.append(self.state[:,i])
+            diag1.append(self.state[i,i])
+            diag2.append(self.state[i,size-i])
         rows.append(diag1)
         rows.append(diag2)
-        for row in rows
+        for row in rows:
             player = row[0]
             if all(map(lambda i: i == player, row[0:])):
                 return player
             #eliminate possible rows?
+
+ticTac = Board()
+ticTac.display()
+ticTac.act((1,2))
+ticTac.display()
