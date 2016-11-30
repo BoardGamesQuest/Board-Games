ee123bf09239c91699a2e035f3ee184026e3e2ed
Osher Lerner <oshlern@nuevaschool.org>
2016-11-29 16:31:44 -0800

    Fixed compiled board info and agents (including a new human agent).

--- Tic Tac Toe/3x3.py
+++ Tic Tac Toe/3x3.py
@@ -1,15 +1,20 @@
 import numpy as np
 from abc import ABCMeta, abstractmethod
+import re
 
 class Board:
-    def __init__(self, agents, numPlayers=2, size=3, dimension=2, debug=False, limit=10):
-        self.agents, self.numPlayers, self.size, self.dimension, self.limit = agents, numPlayers, size, dimension, limit
-        self.state = np.zeros(tuple([size]*dimension), dtype=np.int)
+    def __init__(self, agents, boardParams=[2, 3, 2, 10], debugMode=False):
+        if type(boardParams) == hash:
+            self.numPlayers, self.size, self.dimension, self.limit = boardParams["numPlayers"], boardParams["size"], boardParams["dimension"], boardParams["limit"] # **boardParams
+        elif type(boardParams) == list:
+            self.numPlayers, self.size, self.dimension, self.limit = boardParams[0], boardParams[1], boardParams[2], boardParams[3] # *boardParams
+        self.agents = agents #work on initializing agents
+        self.state = np.zeros(tuple([self.size]*self.dimension), dtype=np.int)
         self.rowIndices = self.findRowIndeces()
         self.emptyIndices = self.rowIndices[:self.size] # Unfinished. Quick way to know which spaces are empty
-        print self.run(doPrint=debug)
+        print self.run(doPrint=debugMode)
 
-    def compileAgents
+    # def compileAgents
 
     def findRowIndeces(self):
         # Compile a list of all the rows, where each row is represented by a list of the positions of its elements
@@ -94,8 +99,9 @@ class Board:
 class Agent:
     # A structure for agents. Your program should inherit this (as seen in randomChoose), meaning that it should
     __metaclass__ = ABCMeta
-    def __init__(self, numPlayers, size, dimension, limit, debug=False):
+    def __init__(self, numPlayers, size, dimension, limit, debugMode=False):
         self.numPlayers, self.size, self.dimension, self.limit = numPlayers, size, dimension, limit
+        self.debugMode = debugMode
 
     @abstractmethod
     def action(self, state, turn, playerNum):
@@ -103,21 +109,42 @@ class Agent:
 
 
 class randomChoose(Agent):
-    def __init__(self, boardParams, debug=False):
-        super().__init__(*boardParams, debug=debug)
+    def __init__(self, boardParams, debugMode=False):
+        print boardParams
+        super(randomChoose, self).__init__(*boardParams, debugMode=debugMode)
 
     def action(self, state, turn, playerNum):
         for i in range(self.size):
-            for j in range(self.size):s
+            for j in range(self.size):
                 if state[i,j] == 0:
-                    if debug: print i,j
+                    if self.debugMode: print i,j
                     return (i,j)
+
+class human(Agent):
+    def __init__(self, boardParams, debugMode=False):
+        super().__init__(boardParams, debugMode=debugMode)
+
+    def action(self, state, turn, playerNum):
+        print "Current State"
+        print self.state
+        userInput = raw_input('It\'s turn {}. Your Move, Player {}: '.format(turn, playerNum))
+        # Cleaning input to standardized form
+        userInput.replace(userInput, r"$[0-9]* $[0-9]*", r"\1,\2") # fix for higher dimension
+        userInput = re.replace(userInput, r"\[\] ", "")
+        position = userInput.split(',')
+        for i in range(len(position)):
+            position[i] = int(position[i])
+        return position
+
+
 agents = []
-boardParams = []
+boardParams = [2, 3, 2, 10]
+# [numPlayers=2, size=3, dimension=2, limit=10]
+# boardParams = {"numPlayers" : 2, "size" : 3, "dimension" : 2, "limit" : 10}
 for i in range(3):
     agents.append(randomChoose(boardParams))
 print agents
-ticTac = Board(agents, numPlayers=3, debug=True)
+ticTac = Board(agents, boardParams, debugMode=True)
 # ticTac.display()
 # ticTac.act((1,1))
 ticTac.display()
