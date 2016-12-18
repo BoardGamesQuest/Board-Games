690f289147e9d660e4464772458672655d593ccf
Osher Lerner <oshlern@nuevaschool.org>
2016-12-17 22:47:15 -0800

    Added efficient, case-specific training to test results (no bueno)

--- Tic Tac Toe/qLearn.py
+++ Tic Tac Toe/qLearn.py
@@ -29,7 +29,7 @@ class Q(Agent):
         return newState
 
     def bestQ(self, state, possibleMoves, returnVal=False):
-        bestMove, bestVal = random.choice(possibleMoves), 0
+        bestMove, bestVal = random.choice(possibleMoves), -1000000000000000
         for move in possibleMoves:
             val = self.Q.get((str(state), move), 0)
             if val > bestVal:
@@ -41,7 +41,7 @@ class Q(Agent):
 
     def updateQVal(self, state, move, val):
         currentVal = self.Q.get((str(state), move), 0)
-        self.Q[str(state), move] = currentVal + self.learningRate * (val - currentVal)
+        self.Q[(str(state), move)] = currentVal + self.learningRate * (val - currentVal)
 
     def action(self, state, turn, playerNum, possibleMoves):
         state = self.boolState(state, playerNum)
@@ -106,3 +106,75 @@ class Q(Agent):
         test = Board(self.boardParams, debugMode=True)
         test.setAgents(agents)
         test.runGames()
+
+    def efficientTrain(self, numGames):
+        # Just 1st player rn
+        winners = []
+        for i in xrange(numGames):
+            if i % 1000 == 0:
+                print "RUNNING GAME: ", i
+            # random.shuffle(agents)
+            # winners.append(self.run())
+            currentPlayer, winner, move = 1, 0, (0,0)
+            state = np.zeros(tuple([self.size]*self.dimension), dtype=np.int)
+            emptyIndices = [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)]
+            for turn in xrange(4):
+                move = random.choice(emptyIndices)
+                if random.random() > 0.8: #self.randomness:
+                    bestVal = -100000000
+                    strState = str(state)
+                    for tempMove in emptyIndices:
+                        val = self.Q.get((strState, tempMove), 0)
+                        if val > bestVal:
+                            move, bestVal = tempMove, val
+                oldStateStr = str(state)
+                state[move] = 1
+                emptyIndices.remove(move)
+                if state[(move[0], 0)] == state[(move[0], 1)] == state[(move[0], 2)]:
+                    winner = 1
+                    break
+                if state[(0, move[1])] == state[(1, move[1])] == state[(2, move[1])]:
+                    winner = 1
+                    break
+                if move[0] == move[1]:
+                    if state[(0, 0)] == state[(1, 1)] == state[(2, 2)]:
+                        winner = 1
+                        break
+                if move[0] == 2 - move[1]:
+                    if state[(0, 2)] == state[(1, 1)] == state[(2, 0)]:
+                        winner = 1
+                        break
+
+                newStateStr = str(np.negative(state))
+                bestVal = 0
+                for move in emptyIndices:
+                    val = self.Q.get((newStateStr, move), 0)
+                    if val > bestVal:
+                        bestVal = val
+                val = np.multiply(-self.discountRate, bestVal)
+                currentVal = self.Q.get((oldStateStr, move), 0)
+                self.Q[(oldStateStr, move)] = currentVal + self.learningRate * (val - currentVal)
+
+                random.shuffle(emptyIndices)
+                move = emptyIndices.pop()
+                state[move] = -1
+                if state[(move[0], 0)] == state[(move[0], 1)] == state[(move[0], 2)]:
+                    winner = -1
+                    break
+                if state[(0, move[1])] == state[(1, move[1])] == state[(2, move[1])]:
+                    winner = -1
+                    break
+                if move[0] == move[1]:
+                    if state[(0, 0)] == state[(1, 1)] == state[(2, 2)]:
+                        winner = -1
+                        break
+                if move[0] == 2 - move[1]:
+                    if state[(0, 2)] == state[(1, 1)] == state[(2, 0)]:
+                        winner = -1
+                        break
+            oldState = np.negative(state)
+            oldState[move] = 0
+            strState = str(oldState)
+            self.Q[(strState, move)] = winner
+            winners.append(winner)
+        return winners
--- Tic Tac Toe/test.py
+++ Tic Tac Toe/test.py
@@ -6,18 +6,21 @@ from qLearn import Q
 
 
 # deterministic = Q(boardParams, randomness=0.001)
-# normal = Q(boardParams, randomness=0.2)
+normal = Q(boardParams)
+normal.efficientTrain(100000)
+print normal.test(500)
+normal.interactiveTest()
 # rand = Q(boardParams, randomness=0.8)
 # agents = [deterministic, normal, rand]
 
 
-print "learningRate, discountRate, originalPerformance, finalPreformannce"
-for learningRate in [0.001, 0.01, 0.1, 1]: #0.001 is really bad with 200 iterations
-    for discountRate in [0.5, 0.75, 1]:
-        agent = Q(boardParams, learningRate=learningRate, discountRate=discountRate)
-        originalPerformance = agent.test(50)
-        agent.randomness = 0.8
-        agent.train(20000)
-        agent.randomness = 0
-        finalPreformannce = agent.test(500)
-        print learningRate, discountRate, originalPerformance, finalPreformannce
+# print "learningRate, discountRate, originalPerformance, finalPreformannce"
+# for learningRate in [0.001, 0.01, 0.1, 1]: #0.001 is really bad with 200 iterations
+#     for discountRate in [0.5, 0.75, 1]:
+#         agent = Q(boardParams, learningRate=learningRate, discountRate=discountRate)
+#         originalPerformance = agent.test(50)
+#         agent.randomness = 0.8
+#         agent.train(20000)
+#         agent.randomness = 0
+#         finalPreformannce = agent.test(500)
+#         print learningRate, discountRate, originalPerformance, finalPreformannce
