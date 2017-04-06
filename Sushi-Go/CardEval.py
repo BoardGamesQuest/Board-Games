0725a4fcaaeb19b4ca438559e174fefcfbe4034f
Shaheen Cullen-Baratloo <shacull@nuevaschool.org>
2017-04-05 17:14:40 -0700

    fixed stuff

--- Sushi-Go/CardEval.py
+++ Sushi-Go/CardEval.py
@@ -36,7 +36,7 @@ class CardEvaluator(Abstract):
             tempPrevHands = copy.deepcopy(self.prevHands)
             tempPrevHands[self.handTracker].remove(card)
             totalScore = 0
-            for hand in tempprevHands:
+            for hand in tempPrevHands:
                 tempuraTracker = 0
                 for card in hand:
                     if (card.cardType == "Tempura"):
@@ -46,4 +46,4 @@ class CardEvaluator(Abstract):
                     score = 2.5
                 totalScore = (totalScore+score)/(1+totalScore*score/2.5^2)
             return totalScore
-        elif (card.cardType == "Sashimi")
+        # elif (card.cardType == "Sashimi"):
